4c60fe0114b3c4369d2280c379be20158c6d8241
MB <mb@mbs-imac.local>
2019-03-25 15:31:35 +0530

    Flip Card Logic Done

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -11,15 +11,25 @@ import UIKit
 
 //UIViewController Consist of logic of controlling all UI Elements so when we extend this superclass we dont have to worry about controlling of UI
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    }
-
-    @IBAction func touchCard(_ sender: UIButton) {
-        print("agh! ghost!")
+        
     }
     
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
+    }
+    
+    func flipCard(withEmoji emoji : String,on button : UIButton){
+        if button.currentTitle == emoji{
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
+        }
+        else{
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
 }
 
