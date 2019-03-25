186e1ba098c4dbf3b548eb88944ca9a479fcd2d1
MB <mb@mbs-imac.local>
2019-03-25 15:47:07 +0530

    Add second Button and FlipCount label Info about IBAction and IBOutlet and right click on UI element to check connections

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -5,23 +5,33 @@
 //  Created by MB on 3/25/19.
 //  Copyright © 2019 MB. All rights reserved.
 //
-
+// Swift is Strongly typed And Stongly Inferred Language
 //UIKit consist of all UI Elements like UIButton
 import UIKit
 
+//IBAction creates a method
+//IBOutlet Creates and instance variable
 //UIViewController Consist of logic of controlling all UI Elements so when we extend this superclass we dont have to worry about controlling of UI
 class ViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
+    // instance variable / property
+    var flipCount : Int = 0
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
     
     @IBAction func touchCard(_ sender: UIButton) {
+        flipCount+=1
+        flipCountLabel.text = "Flips: \(flipCount)"
         flipCard(withEmoji: "👻", on: sender)
+    }
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCount+=1
+        flipCountLabel.text = "Flips: \(flipCount)"
+        flipCard(withEmoji: "🎃", on: sender)
     }
     
     func flipCard(withEmoji emoji : String,on button : UIButton){
+      //  print("flipCard(withEmoji : \(emoji))")
         if button.currentTitle == emoji{
             button.setTitle("", for: .normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
