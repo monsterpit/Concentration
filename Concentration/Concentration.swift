20ef09ab26dfd719813480db1fdbad151f6439a6
MB <mb@mbs-imac.local>
2019-03-25 18:19:25 +0530

    Logic of Flipping cards in Model (Using Optional to track the state of indexOfOneAndOnlyFaceUpCard if its set or not set)

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -13,18 +13,44 @@ class Concentration
 {
     var cards = [Card]()
     
+    var indexOfOneAndOnlyFaceUpCard : Int?
+    
     func chooseCard(at index : Int){
-        if cards[index].isFaceUp{
-            cards[index].isFaceUp = false
+        //checking if cards aint matched
+        if !cards[index].isMatched{
+            
+            //checking if indexOfOneAndOnlyFaceUpCard have value and if current index is not equals to it
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard,matchIndex != index{
+                //check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier{
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                // turning second card up irrespective of if itsd matched or not
+                cards[index].isFaceUp = true
+                
+                //setting indexOfOneAndOnlyFaceUpCard to nil as 2 cards are up now
+                indexOfOneAndOnlyFaceUpCard = nil
+            }
+            else{
+                // either no cards or 2 cards are faceup
+                for flipDownIndex in cards.indices{
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                
+                // flipping selected card up
+                cards[index].isFaceUp = true
+                
+                // setting indexOfOneAndOnlyFaceUpCard to indexd of card
+                indexOfOneAndOnlyFaceUpCard = index
+            }
         }
-        else{
-            cards[index].isFaceUp = true
-        }
+        
     }
     
     init(numberOfPairsOfCards : Int){
         for _ in 1...numberOfPairsOfCards{
-
+            
             let card = Card()
             cards += [card,card]
         }
