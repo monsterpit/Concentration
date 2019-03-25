42cbc8c9750b4ecbf7456867f73496af65f2efef
MB <mb@mbs-imac.local>
2019-03-25 13:52:51 +0530

    Added a button with Action

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -6,15 +6,20 @@
 //  Copyright © 2019 MB. All rights reserved.
 //
 
+//UIKit consist of all UI Elements like UIButton
 import UIKit
 
+//UIViewController Consist of logic of controlling all UI Elements so when we extend this superclass we dont have to worry about controlling of UI
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-
+    @IBAction func touchCard(_ sender: UIButton) {
+        print("agh! ghost!")
+    }
+    
 }
 
